f930ea34dbf621c41bfddcb11ae6cfa1c2ad524b
Serhii Lesiuk <seranatles@gmail.com>
2021-01-07 17:37:08 +0200

    тут ничего интересного

---
+++ resources/views/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" href="{{ route('Home') }}">Главная</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('StatUrl') }}">Статистика переходов</a>
+        </li>
+    </ul>
+</header>
